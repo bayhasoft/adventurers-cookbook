4f554913b06c543befd58650b65f564dec7b20b3
KG7PNH <kg7pnh@arrl.net>
2023-03-26 15:58:01 -0500

    added fabric template and updated all relevant values

---
+++ src/main/java/bayhasoft/adventurerscookbook/AdventurersCookBookClient.java
@@ -0,0 +1,10 @@
+package bayhasoft.adventurerscookbook;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdventurersCookBookClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
