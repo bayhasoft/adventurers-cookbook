76372dd601146b91e17f5c6d6be6b4863e1cd376
KG7PNH <kg7pnh@arrl.net>
2023-11-26 17:21:37 -0600

    Removed the composting mixin as it was causing issues with world rendering; commented out some unused methods and imports; set version to 0.1.6

--- src/main/java/bayhasoft/adventurerscookbook/item/ModItems.java
+++ src/main/java/bayhasoft/adventurerscookbook/item/ModItems.java
@@ -11,7 +11,7 @@ import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.Items;
+// import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
